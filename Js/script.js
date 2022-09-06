8210541f84d38307d3e6e0bc12c48ad1d69dfeef
Mariano Méndez <marianoezequielmendez@gmail.com>
2022-09-06 11:21:48 -0300

    [UPD] Reorganización de código base

--- Js/script.js
+++ Js/script.js
@@ -1,16 +1,19 @@
 // VARIABLES
-let height = parseFloat(prompt("¿Cuál es su altura?"));
-let weight = parseFloat(prompt("¿Cuál es su peso?"));
-let gender = prompt(
-  "¿Cuál es su género? (H(Hombre), M(Mujer), O(Otro))"
-).toUpperCase();
+let height;
+let weight;
+let gender;
 let genderCalculator = 0;
 let checker = true;
+let calcular;
 
 // FUNCIONES
-let calcularVolumen = (height, weight, genderCalculator) => {
-  // Calcula el volumen total del agua a tomar, según parámetros.
-  return ((height / weight) * genderCalculator).toFixed(2);
+let asignaValores = () => {
+  // Asigna valores iniciales
+  height = parseFloat(prompt("¿Cuál es su altura?"));
+  weight = parseFloat(prompt("¿Cuál es su peso?"));
+  gender = prompt(
+    "¿Cuál es su género? (H(Hombre), M(Mujer), O(Otro))"
+  ).toUpperCase();
 };
 
 let genderCheck = (checker, gender) => {
@@ -27,28 +30,39 @@ let genderCheck = (checker, gender) => {
       checker = false;
     } else {
       gender = prompt(
-        "¿Cuál es su género? (H(Hombre), M(Mujer), O(Otro))"
+        "Debé seleccionar uno de los siguientes géneros: H(Hombre), M(Mujer), O(Otro)"
       ).toUpperCase();
     }
   }
   return checker;
 };
 
-// RESULTADOS
-genderCheck(checker, gender);
-let calcular = confirm("¿Desea calcular su volumen?");
+let calcularVolumen = (height, weight, genderCalculator) => {
+  // Calcula el volumen total del agua a tomar, según parámetros.
+  return ((height / weight) * genderCalculator).toFixed(2);
+};
 
-if (calcular) {
-  alert(
-    `Ustede debe beber ${calcularVolumen(
-      height,
-      weight,
-      genderCalculator
-    )} lts de agua por día.`
-  );
-} else {
-  alert("Gracias por su visita");
-}
+let resultado = () => {
+  calcular = confirm("¿Desea calcular su volumen?");
+
+  if (calcular) {
+    alert(
+      `Ustede debe beber ${calcularVolumen(
+        height,
+        weight,
+        genderCalculator
+      )} lts de agua por día.`
+    );
+  } else {
+    alert("Gracias por su visita");
+  }
+};
+
+// LLAMAR FUNCIONES
+asignaValores();
+genderCheck(checker, gender);
+calcularVolumen(height, weight, genderCalculator);
+resultado();
 
 // FALTA POR HACER
 // Agregar interacción con los botones para ir calculando el agua bebida hasta el momento.
